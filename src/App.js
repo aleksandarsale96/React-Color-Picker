1a46ca31c218f2bee094dc9f0959b56edb62a191
salelibertas <salelibertas@gmail.com>
2022-08-06 13:23:44 +0200

    color-palette

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./index.css";
+
+export default function App() {
+  const colors = [
+    "#9253a1",
+    "#f063a4",
+    "#2dc5f4",
+    "#fcee21",
+    "#f16164",
+    "#70327e",
+    "#a42963",
+    "#0b6a88",
+    "#f89e4f",
+    "#ec015a"
+  ];
+  
+  const [background, setBackground] = useState("#071415");
+  const [current, setCurrent] = useState(null);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setCurrent(null);
+    }, 3000);
+    return () => clearTimeout(timeoutId);
+  }, [current]);
+
+  return (
+    //definisanje background-a na samom pocetku 
+    <div className="App" style={{ background: background }}>
+     
+      <div className="container">
+        {colors.map((color, index) => (
+          <div key={index} className="card">
+            <div
+              style={{
+                background: color,
+                filter: "brightness(85%)",
+                boxShadow: color === background ? "0 0 5px #000" : ""
+              }}
+              className="box"
+              onClick={() => setBackground(color)}
+            />
+            <CopyToClipboard text={`color: ${color};`}>
+              <p
+                style={{ color: color === background ? "#fff" : color }}
+                onClick={() => setCurrent(color)}
+              >
+                {color}
+              </p>
+            </CopyToClipboard>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
